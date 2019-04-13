123a9ecf428327a7f4d7978d81fc81858e7ab43d
Pavel Khaskel <haskel.pavel@gmail.com>
2019-04-13 23:24:57 +0700

    init

---
+++ DependencyInjection/MapSerializerExtension.php
@@ -0,0 +1,13 @@
+<?php
+namespace Haskel\MapSerializerBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class MapSerializerExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+
+}
---
+++ MapSerializerBundle.php
@@ -0,0 +1,9 @@
+<?php
+namespace Haskel\MapSerializerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MapSerializerBundle extends Bundle
+{
+
+}
